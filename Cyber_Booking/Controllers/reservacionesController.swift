9d508c55830a26b959f37d2a349ade90eb3db3f8
AbielBorja <abielboga@gmail.com>
2022-09-01 20:46:07 -0700

    Botones y tranciones
    Puse algunos botones y transiciones

---
+++ Cyber_Booking/Controllers/reservacionesController.swift
@@ -0,0 +1,18 @@
+//
+//  reservacionesController.swift
+//  Cyber_Booking
+//
+//  Created by Abiel on 9/1/22.
+//
+
+import Foundation
+import UIKit
+
+class reservacionesController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dismissTap(_ sender: Any) {
+        dismiss(animated: true, completion: nil)    }
+}
