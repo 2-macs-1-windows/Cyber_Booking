e987fb77592dfe756a8f3e27d4bf3f78b477ec45
AbielBorja <abielboga@gmail.com>
2022-10-02 16:19:56 -0700

    Static table hw

---
+++ Cyber_Booking/Controllers/ReservasHardwareTableViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ReservasHardwareTableViewController.swift
+//  Cyber_Booking
+//
+//  Created by user224407 on 10/2/22.
+//
+
+import UIKit
+
+class ReservasHardwareTableViewController: UITableViewController {
+
+    
+    var reservasHw = ReserveHw.listaReserveHw()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return reservasHw.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "zeldaHw", for: indexPath) as! ReservacionesHardwareTableViewCell
+
+        // Configure the cell...
+        let indice = indexPath.row
+        let reserva = reservasHw[indice]
+        cell.update(r: reserva)
+        
+        cell.showsReorderControl = true
+
+        return cell
+    }
+    
+    @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
+        let tableViewEditingMode = tableView.isEditing
+            tableView.setEditing(!tableViewEditingMode, animated: true)
+        
+    }
+    
+    
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            reservasHw.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let movedEmoji = reservasHw.remove(at: fromIndexPath.row)
+        reservasHw.insert(movedEmoji, at: to.row)
+
+    }
+    
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
