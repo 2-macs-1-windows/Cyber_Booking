ceb574aa357665be380c07b08a1623eb10199e68
Victoria Vazquez <65380353+VickyA01654095@users.noreply.github.com>
2022-10-07 16:13:21 -0500

    logout en app

--- Cyber_Booking/Controllers/PerfilViewController.swift
+++ Cyber_Booking/Controllers/PerfilViewController.swift
@@ -12,6 +12,40 @@ class PerfilViewController: UIViewController {
     // Sacar el id del usuario como appDelegate.user_id 
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
+    let userLog = "http://127.0.0.1:8000/logoutApp"
+    
+    struct answer: Codable {
+        var msg: String
+        var id: Int?
+    }
+    
+    func logout() async throws->answer{
+        let insertURL = URL(string: userLog)!
+        var request = URLRequest(url: insertURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(["id": appDelegate.user_id])
+        request.httpBody = jsonData
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else { throw ReservaError.itemNotFound}
+        
+        let jsonDecoder = JSONDecoder()
+        do{
+        
+            let reservas = try jsonDecoder.decode(answer.self, from: data)
+            
+            return reservas
+            
+        }catch let jsonError as NSError{
+            
+            print("JSON decode failed: \(jsonError)")
+            throw ReservaError.decodeError
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,7 +54,23 @@ class PerfilViewController: UIViewController {
     
 
     @IBAction func logoutFunc(_ sender: Any) {
-        
+        Task{
+            do{
+                let is_logIn = try await logout()
+                
+                let storyboard = UIStoryboard(name:"Main", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "MainPage") as UIViewController
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated:true, completion:nil)
+                
+            }catch let jsonError as NSError{
+                print("JSON decode failed: \(jsonError)")
+                let alert = UIAlertController(title: "Error de conexión", message: "No fue posible obtener la lista de usuarios", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Aceptar", style: .cancel, handler:  nil))
+                
+                self.present(alert, animated: true, completion: nil)            }
+        }
     }
     
     
